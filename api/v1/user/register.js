2d4a124e9511637475d02798ff32f698c12be313
jaswinder <kumarsanju54@gmail.com>
2021-09-15 10:26:52 +0530

    all code

---
+++ api/v1/user/register.js
@@ -0,0 +1,87 @@
+/*
+ * @file: register.js
+ * @description: It Contain register router/api.
+ * @author: Jaswinder Kumar
+ */
+import express from "express";
+import { createValidator } from "express-joi-validation";
+import Joi from "@hapi/joi";
+import { register } from "../../../controllers/user";
+import { DEVICE, ROLE } from "../../../utilities/constants";
+const app = express();
+const validator = createValidator({ passError: true });
+
+/**
+ * @swagger
+ * /api/v1/register:
+ *  post:
+ *   tags: ["Auth"]
+ *   summary: user login api
+ *   description: api used to login users <br/>
+ *   parameters:
+ *      - in: body
+ *        name: user
+ *        description: The user to login.
+ *        schema:
+ *         type: object
+ *         required:
+ *          - user login
+ *         properties:
+ *           firstName:
+ *             type: string
+ *             required:
+ *           lastName:
+ *             type: string
+ *             required:
+ *           email:
+ *             type: string
+ *             required:
+ *           password: 
+ *             type: string
+ *             required:  
+ *           timeZone:
+ *             type: string
+ *           country:
+ *             type: string             
+ *   responses:
+ *    '200':
+ *      description: success
+ *    '400':
+ *      description: fail
+ */
+
+const userSchema = Joi.object({
+  firstName: Joi.string()
+    .required()
+    .label("First Name"),
+  lastName: Joi.string()
+    .required()
+    .label("Last Name"),
+  email: Joi.string()
+    .trim()
+    .required()
+    .label("email"),
+  password: Joi.string()
+    .trim()
+    .min(3)
+    .required()
+    .label("password"),
+  timeZone: Joi.any()
+    .optional()
+    .allow("")
+    .label("timeZone"),
+  country: Joi.string()
+    .optional()
+    .allow("")
+    .label("country")
+});
+
+app.post(
+  "/register",
+  validator.body(userSchema, {
+    joi: { convert: true, allowUnknown: false }
+  }),
+  register
+);
+
+export default app;
